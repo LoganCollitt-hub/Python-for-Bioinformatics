4ade6e5b5c3fca3be344e751cc2317d453023314
LoganCollitt-hub <logancollitt2009@gmail.com>
2026-08-25 20:22:20 +0100

    Create Reverse-complement.py

---
+++ Reverse-complement.py
@@ -0,0 +1,20 @@
+def get_complement(dna_sequence):
+# Defines a dictionary to map each nucleotide to its complementary base.
+    complement_dict = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+ # Iterates through each base in the DNA sequence and stores its complement.
+    complement_list = [complement_dict[base] for base in dna_sequence]
+# Joins the list of complementary bases into a single string.
+    return ''.join(complement_list)
+
+def get_reverse_complement(dna_sequence):
+# Gets the complement of the DNA sequence
+    complement_list = get_complement(dna_sequence)
+# Reverses the complemented sequence.
+    reverse_complement_list = complement_list[::-1]
+# Joins the reversed bases into a new string.
+    reverse_complement = ''.join(reverse_complement_list)
+    return reverse_complement
+
+# Calls the complement and reverse complement functions and stores the results.
+complement = get_complement(dna_sequence)
+reverse_complement = get_reverse_complement(dna_sequence)
