d2c5064b7a47857d1b665e1e9c90ecf340723619
LoganCollitt-hub <logancollitt2009@gmail.com>
2026-08-26 00:41:32 +0100

    Update Nucleotide-frequencies.py

--- Nucleotide-frequencies.py
+++ Nucleotide-frequencies.py
@@ -1,3 +1,6 @@
+############################################################
+# Function: Calculate nucleotide frequency
+############################################################
 def nucleotide_frequency(dna_sequence):
 # Initializes a dictionary to store the counts for each of the four standard nucleotides.
     freq = {'A': 0, 'T': 0, 'C': 0, 'G': 0}
@@ -5,7 +8,9 @@ def nucleotide_frequency(dna_sequence):
     for char in dna_sequence:
         freq[char] += 1
     return freq
-
+############################################################
+# Function: Calculate nucleotide percentage
+############################################################
 def nucleotide_percentage(dna_sequence):
 # Retrieves the nucleotide frequency dictionary for the given DNA sequence.
     freq = nucleotide_frequency(dna_sequence)
@@ -14,7 +19,7 @@ def nucleotide_percentage(dna_sequence):
     for nucleotide, count in freq.items():
        percentage = (count / total_length) * 100
        print(f"Nucleotide percentage of {nucleotide} is:{percentage:.2f}%")
-
+========================================================================================================================
 freq = nucleotide_frequency(dna_sequence)
 freq_string =','.join(f" {base}:{count}" for base, count in freq.items())
 print(f"Nucleotide frequencies:{freq_string}")
