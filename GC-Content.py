4a1503b9524d68273502a5ec5c5923eaf36c55c8
MSc Bioinformatics <logancollitt2009@gmail.com>
2026-08-26 23:27:46 +0100

    Update GC-Content.py

--- GC-Content.py
+++ GC-Content.py
@@ -3,8 +3,7 @@
 ############################################################
 def calculate_gc_content (dna_sequence):
     freq = nucleotide_frequency(dna_sequence)
-# Retrieves the nucleotide counts and extracts the combined total of G and C bases.
     gc_count = freq["G"] + freq["C"]
-# Calculates GC content by dividing the GC count by the total sequence length.
     gc_content = (gc_count / len(dna_sequence)) * 100
-    print(f"GC content is: {gc_content:.2f}%")
+############################################################
+print(f"GC content is: {gc_content:.2f}%")
