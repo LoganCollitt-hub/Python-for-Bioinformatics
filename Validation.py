2af53ee7b587d55e2dcb355ff80e397039802a95
MSc Bioinformatics <logancollitt2009@gmail.com>
2026-08-26 23:26:58 +0100

    Update Validation.py

--- Validation.py
+++ Validation.py
@@ -1,28 +1,12 @@
-#Example DNA Sequence
-dna_sequence = (
-    "ATGCGTACGTTAGCTAGCGATCGATGCTAACGTTAGCGGATCCATGAACTGCTAGCTAACGATCGTACG"
-    "ATGCCGTTAGCATCGATGCTAGGCTAACCGATCGTACGATGAACTAGCTAGCGTACCGATGCTAACGT"
-    "TAGCGATCCGATGAACTGCTAGCATGCGTACGTTAGCTAACGATCGGCTAGCTAGCATGAACTGCGTA"
-    "CCGATGCTAACGTTAGCGATCGATGCCGTTAGCTAGCATGAACTGCTAACCGATCGTACGATGCTAG"
-    "CTAGCGTTAGCATGCGTACGATCGATGAACTGCTAGCTAACGTTAGCGATCCGATGCTAGCATGAACT"
-    "GCGTACCGATGCTAACGTTAGCTAGCGATCGATGCCGTTAGCATGAACTGCTAGCTAACCGATCGTAC"
-)
-
-# Converts the sequence to uppercase to ensure consistent processing.
-dna_sequence = dna_sequence.upper().replace('',"")
-
 ############################################################
 # Function: DNA Sequence Validation 
 ############################################################
 def validate_sequence(dna_sequence):
-# Check if the sequence is empty
     if dna_sequence == "":
           print("Sequence cannot be empty!")
     elif set(dna_sequence) <= {"A","T","C","G"}:
-# If all characters in the sequence are valid nucleotides, the sequence is accepted.
         print("Valid Sequence!")
     else:
-# If any character is not a valid nucleotide, the sequence is rejected.
         print("Invalid Sequence!")
 ############################################################
 validate_sequence(dna_sequence)
