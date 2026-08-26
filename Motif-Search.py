76daa4ec846bac33af8bfdb8c6c948bb4cfe5f3c
MSc Bioinformatics <logancollitt2009@gmail.com>
2026-08-26 23:22:27 +0100

    Update Motif-Search.py
    indexing.

--- Motif-Search.py
+++ Motif-Search.py
@@ -1,27 +1,20 @@
 ############################################################
 # Function: Motif Search
 ############################################################
-# Function to find DNA motif
+
 def find_motif(dna_sequence, motif):
-# Initializes an empty list to store the starting indices where the motif is found (zero-based).
-    zero_based=[]
-# Iterates through the DNA sequence and checks each position for a matching motif.
+    zero_based=[] 
     for i in range(len(dna_sequence) - len(motif)+1):
-# Extracts a segment of the DNA sequence equal in length to the motif.
         if dna_sequence[i:i+len(motif)] == motif:
-# If a match is found, adds the current starting index to the list.
             zero_based.append(i)
-# Converts zero-based positions into one-based positions.
     one_based = [i + 1 for i in zero_based]
-# Calculates the distance between each consecutive motif occurrence.
     distances = [zero_based[i+1] - zero_based[i]for i in range(len(zero_based) - 1)]
-# Finds the shortest and longest distances between motif occurrences.
+
     shortest = min(distances)
     longest = max(distances)
 
     return zero_based, one_based, distances, shortest, longest
 ========================================================================================================================
-# Defines a smotif to search for.
 motif = "ATG"
 zero_based, one_based, distances, shortest, longest = find_motif(dna_sequence,motif)
 
