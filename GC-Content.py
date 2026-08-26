f02dcda5a2108e0f1d73f91fb612ff28af5c7a98
MSc Bioinformatics <logancollitt2009@gmail.com>
2026-08-26 23:34:02 +0100

    Fix indentation and formatting in GC-Content.py

--- GC-Content.py
+++ GC-Content.py
@@ -5,5 +5,5 @@ def calculate_gc_content (dna_sequence):
     freq = nucleotide_frequency(dna_sequence)
     gc_count = freq["G"] + freq["C"]
     gc_content = (gc_count / len(dna_sequence)) * 100
-############################################################
+========================================================================================================================
 print(f"GC content is: {gc_content:.2f}%")
