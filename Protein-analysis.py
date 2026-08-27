b1e3a3f763adafc891afc19249f624b0adca646c
MSc Bioinformatics <logancollitt2009@gmail.com>
2026-08-27 12:31:59 +0100

    Update Protein-analysis.py
    Refactor protein analysis functions and remove unused code.

--- Protein-analysis.py
+++ Protein-analysis.py
@@ -9,6 +9,9 @@ def count_amino_acids(protein_sequence):
     for aa in protein_sequence:
         aa_counts[aa] = aa_counts.get(aa, 0) + 1
     return aa_counts
+
+aa_sorted = dict(sorted(count_amino_acids(protein).items()))
+print(f"Amino Acid: {aa_sorted}")
 ############################################################
 # Function: Most common Amino acid 
 ############################################################
@@ -17,21 +20,15 @@ def most_common_aa(aa_sorted):
     for aa in aa_sorted:
         if aa_sorted[aa] == highest_count:
             return aa, highest_count
-========================================================================================================================
-   
-aa_sorted = dict(sorted(count_amino_acids(protein).items()))
-
-print("\n======= Protein Analysis =======\n")
-for aa, count in aa_sorted.items():
-    print(f"Amino Acid: '{aa}': {count}")
-
-print("\n======= Most Common Amino Acid =======\n")
+            
 most_common, highest_count = most_common_aa(aa_sorted)
-percentage_aa = (highest_count / len(protein)) * 100
 print(f"Most common amino acid: {most_common}")
-print(f"Percentage: {percentage_aa:.2f}%")
-
-print("\n======= Stop Codon Frequency =======\n")
-stop_codons = protein.count("*")
-print(f"Stop Codon Count: {stop_codons}")
+############################################################
+# Function: Amino Acid Composition
+############################################################
+def amino_comp(protein_sequence):
+    aa_counts = count_amino_acids(protein_sequence)
+    for aa in protein_sequence:
+        aa_percent = (aa_counts[aa] / len(protein_sequence)) * 100
+        print(aa, ":", f"{aa_percent:.2f}%")
 
