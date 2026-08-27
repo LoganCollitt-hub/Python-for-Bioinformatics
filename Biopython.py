184a25dc7a7816d6da76a029b9e3b0384fcbeff3
MSc Bioinformatics <logancollitt2009@gmail.com>
2026-08-27 12:35:18 +0100

    Refactor DNA analysis, transcription, and translation
    Updated comments and removed unnecessary print statements.

--- Biopython.py
+++ Biopython.py
@@ -2,28 +2,23 @@
 from Bio.Seq import Seq
 ========================================================================================================================
 ############################################################
-#                     START OF PROGRAM                     #
+# Function: DNA Analysis
 ############################################################
-
-print("\n======= DNA Sequence Analysis =======\n")
 dna = Seq(dna_sequence)
 print("DNA Sequence:", repr(dna))
 print(f"Length: {len(dna)}")
-
 ############################################################
-print("\n======= Transcription =======\n")
+# Function: Transcription
+############################################################
 rna = dna.transcribe()
 trim = len(rna) - (len(rna) % 3)
 rna = rna[:trim]
 print("RNA Sequence:", repr(rna))
 print(f"Length: {len(rna)}")
-
 ############################################################
-print("\n======= Translation =======\n")
+# Function: Translation
+############################################################
 protein = rna.translate()
 print("Protein Sequence:", repr(protein))
 print(f"Length: {len(protein)}")
 
-############################################################
-#                       END OF PROGRAM                     #
-############################################################
